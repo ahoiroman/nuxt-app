d095d18b7162be55b7dcc8384b8a8da905bc8a05
rlossin <roman.lossin@icloud.com>
2022-08-12 16:51:43 +0200

    removed old entries

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     build: {
-        transpile: ['@heroicons/vue', /echarts/, '@headlessui/vue', 'vue-echarts'],
+        transpile: [/echarts/, 'vue-echarts'],
     },
 })
